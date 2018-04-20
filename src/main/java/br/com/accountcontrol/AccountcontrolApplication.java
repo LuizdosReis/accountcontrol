ebfe1a87ba4849f710994a962fde226452affe49
Luiz dos Reis <luizdosreis@gmail.com>
2018-04-20 20:02:22 -0300

    first commit

---
+++ src/main/java/br/com/accountcontrol/AccountcontrolApplication.java
@@ -0,0 +1,12 @@
+package br.com.accountcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountcontrolApplication.class, args);
+	}
+}
